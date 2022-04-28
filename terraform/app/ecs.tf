49db0e7b701dd05568ef4217ef9df914f6b51e28
smiroshnichenko <celt@root.in.ua>
2022-04-28 18:40:51 +0300

    debug

--- terraform/app/ecs.tf
+++ terraform/app/ecs.tf
@@ -118,7 +118,7 @@ module "task-definition-frontend" {
   source          = "cn-terraform/ecs-fargate-task-definition/aws"
   name_prefix     = "${var.name_prefix}-frontend"
   container_name  = "${var.name_prefix}-frontend"
-  map_environment = {"APP_BASE_URL":"https://shopizer-api.rootin.cc"}
+  map_environment = {"APP_BASE_URL":"https://shopizer-api.rootin.cc", "APP_MERCHANT":"DEFAULT"}
   port_mappings = [
     {
       "containerPort": 80,
@@ -135,6 +135,29 @@ module "task-definition-frontend" {
     }
   }
   container_image = "${aws_ecr_repository.frontend.repository_url}:${var.image_tag}"
+}
+
+module "task-definition-admin" {
+  source          = "cn-terraform/ecs-fargate-task-definition/aws"
+  name_prefix     = "${var.name_prefix}-admin"
+  container_name  = "${var.name_prefix}-admin"
+  map_environment = {"APP_BASE_URL":"https://shopizer-api.rootin.cc/api"}
+  port_mappings = [
+    {
+      "containerPort": 80,
+      "hostPort": 80,
+      "protocol": "tcp"
+    }
+  ]
+  log_configuration = {
+    "logDriver": "awslogs",
+    "options": {
+        "awslogs-group": "${var.name_prefix}",
+        "awslogs-region": "${var.aws_region}",
+        "awslogs-stream-prefix": "admin"
+    }
+  }
+  container_image = "${aws_ecr_repository.admin.repository_url}:${var.image_tag}"
 }
 
 #------------------------------------------------------------------------------
@@ -343,6 +366,118 @@ resource "aws_route53_record" frontend {
 
 resource "aws_service_discovery_service" "frontend" {
   name = "${var.name_prefix}"
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.app.id
+    routing_policy = "MULTIVALUE"
+    dns_records {
+      ttl = 10
+      type = "A"
+    }
+    dns_records {
+      ttl  = 10
+      type = "SRV"
+    }
+  }
+  health_check_custom_config {
+    failure_threshold = 5
+  }
+}
+
+#------------------------------------------------------------------------------
+# ADMIN ECS Service
+#------------------------------------------------------------------------------
+resource "aws_security_group" admin_security_group {
+  name        = "${var.name_prefix}-admin"
+  description = "${var.name_prefix} admin security group"
+  vpc_id      = data.terraform_remote_state.infra.outputs.vpc.vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    self            = true
+    security_groups = [aws_security_group.alb_security_group.id]
+    from_port       = 80
+    to_port         = 80
+    description     = "Communication channel to admin"
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = local.tags
+}
+
+resource "aws_lb_target_group" admin {
+  name        = replace("${var.name_prefix}-admin", "_", "-")
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.terraform_remote_state.infra.outputs.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    enabled = true
+    path    = "/"
+  }
+  tags       = local.tags
+  depends_on = [aws_lb.this]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener_rule" admin_rule {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 8
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.admin.arn
+  }
+  condition {
+    host_header {
+      values = ["${var.name_prefix}-admin.*"]
+    }
+  }
+}
+
+resource "aws_ecs_service" admin {
+  name             = "${var.name_prefix}-admin"
+  task_definition  = module.task-definition-admin.aws_ecs_task_definition_td_arn
+  cluster          = module.ecs-cluster.aws_ecs_cluster_cluster_arn
+  desired_count    = 1
+  launch_type      = "FARGATE"
+  platform_version = "1.4.0"
+  service_registries {
+    registry_arn  = aws_service_discovery_service.admin.arn
+    port          =  80
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.admin.arn
+    container_name   = "${var.name_prefix}-admin"
+    container_port   = 80
+  }
+  network_configuration {
+    subnets          = data.terraform_remote_state.infra.outputs.vpc.private_subnets
+    security_groups  = [aws_security_group.admin_security_group.id]
+    assign_public_ip = false
+  }
+  depends_on = [aws_lb_listener.https]
+}
+
+
+resource "aws_route53_record" admin {
+  zone_id = var.route53_zone_id
+  name    = "${var.name_prefix}-admin"
+  type    = "A"
+  alias {
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_service_discovery_service" "admin" {
+  name = "${var.name_prefix}-admin"
   dns_config {
     namespace_id = aws_service_discovery_private_dns_namespace.app.id
     routing_policy = "MULTIVALUE"
